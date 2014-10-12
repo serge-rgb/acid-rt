0e71f1dbc23ffd9c0bba013cf919f9954775591d
Sergio Gonzalez <bigmonachus@gmail.com>
2014-10-12 02:13:55 -0500

    remove AllocRecord crap

--- src/ph.cc
+++ src/ph.cc
@@ -5,29 +5,10 @@
 
 namespace ph {
 
-
-#ifdef PH_DEBUG
-struct AllocRecord {
-    void* ptr;
-    size_t size;
-};
-
-static size_t g_total_memory = 0;
-static size_t g_total_allocs = 0;
-
-//Dict<int64, AllocRecord> g_alloc_records;
-// TODO Redo g_alloc_records
-static bool g_init = false;
-#endif
-
 void init() {
 #ifndef PH_SLICES_ARE_MANUAL
     GC_init();
     GC_enable_incremental();
-#endif
-
-#ifdef PH_DEBUG
-
 #endif
 }
 
@@ -111,40 +92,13 @@ void* typeless_alloc(size_t n_bytes) {
     if (!ptr) {
         ph::phatal_error("Allocation failed");
     }
-#ifdef PH_DEBUG
-    if (!g_init) {
-        g_init = true;
-    }
-#if 0
-    AllocRecord r = {ptr, n_bytes};
-    insert(&ph::g_alloc_records, (int64) (ULONGLONG)(ULONG)ptr, r);
-    g_total_memory += n_bytes;
-#endif
-#endif
     return ptr;
 }
 
 void typeless_free(void* mem) {
-#ifdef PH_DEBUG
-#if 0
-    AllocRecord* r = find(&ph::g_alloc_records, (int64)(ULONGLONG)(ULONG)mem);
-    if (!r) {
-        log("ERROR: AllocRecord not found for pointer.");
-    } else {
-        g_total_memory -= r->size;
-    }
-#endif
-#endif
     free(mem);
-}  // ns memory
-
-int64 bytes_allocated() {
-#ifdef PH_DEBUG
-    return (int64)g_total_memory;
-#else
-    return 0;
-#endif
 }
+
 }  // ns memory
 
 void quit(int code) {
