ffc99ceb67c68cba8a14ea37aa453be379e168d7
Sergio Gonzalez <bigmonachus@gmail.com>
2014-11-05 14:29:56 -0600

    Share context with GL

--- ocl_hello/ocl.cc
+++ ocl_hello/ocl.cc
@@ -2,6 +2,8 @@
 
 #include <opencl.h>
 
+#include "window.h"
+
 using namespace ph;
 
 void __stdcall context_callback(
@@ -9,8 +11,15 @@ void __stdcall context_callback(
     logf("OpenCL context error:  %s\n", errinfo);
 }
 
+void ocl_idle() {
+    window::swap_buffers();
+}
+
 int main() {
     ph::init();
+
+    // Creates a GL context, so we need to init the window before doing OpenCL.
+    window::init("OCL", 1920, 1080, window::InitFlag_Default);
 
     // ========================================
     // Get platforms
@@ -78,8 +87,46 @@ int main() {
     cl_device_id device = devices[0];
     cl_int err = 0;
 
+    cl_context_properties* props = NULL;
+
+    // ========================================
+    // Platform specific context creation w/OpenGL ctx sharing
+    // ========================================
+#if defined (__APPLE__)
+    CGLContextObj kCGLContext = CGLGetCurrentContext();
+    CGLShareGroupObj kCGLShareGroup = CGLGetShareGroup(kCGLContext);
+    cl_context_properties local_props[] =
+    {
+        CL_CONTEXT_PROPERTY_USE_CGL_SHAREGROUP_APPLE, (cl_context_properties)kCGLShareGroup,
+        0
+    };
+    //context = clCreateContext(local_props, 0,0, NULL, NULL, &ciErrNum);
+    props = local_props;
+#else
+#ifdef UNIX
+    cl_context_properties local_props[] =
+    {
+        CL_GL_CONTEXT_KHR, (cl_context_properties)glXGetCurrentContext(),
+        CL_GLX_DISPLAY_KHR, (cl_context_properties)glXGetCurrentDisplay(),
+        //CL_CONTEXT_PLATFORM, (cl_context_properties)cpPlatform,
+        0
+    };
+    //context = clCreateContext(local_props, 1, , NULL, NULL, &ciErrNum);
+    props = local_props;
+#else // Win32
+    cl_context_properties local_props[] =
+    {
+        CL_GL_CONTEXT_KHR, (cl_context_properties)wglGetCurrentContext(),
+        CL_WGL_HDC_KHR, (cl_context_properties)wglGetCurrentDC(),
+        CL_CONTEXT_PLATFORM, (cl_context_properties)platforms[0],
+        0
+    };
+    //context = clCreateContext(local_props, 1, &cdDevices[uiDeviceUsed], NULL, NULL, &ciErrNum);
+    props = local_props;
+#endif
+#endif
     cl_context context = clCreateContext(
-            /*properties=*/NULL,
+            props,
             1, &devices[0],
             context_callback,
             /*user_data=*/NULL,
@@ -87,25 +134,48 @@ int main() {
 
     if (err != CL_SUCCESS) {
         logf("err num %d\n", err);
-        phatal_error("Cannot create command queue");
+        phatal_error("Cannot create context");
     }
+
+    // ========================================
+    // ========================================
 
     // ========================================
     // Command queue
     // ========================================
 
-    cl_command_queue queue = clCreateCommandQueue(context, device, CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE, &err);
+    cl_command_queue queue =
+        clCreateCommandQueue(context, device, CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE, &err);
     // ^----- commands can be profiled (spec: 5.12)
-
     if (err != CL_SUCCESS) {
         logf("err num %d\n", err);
-        phatal_error("Cannot create context");
+        phatal_error("Cannot create command queue");
     }
+
+
+    // ========================================
+    // Do stuff before we block and go to main loop
+    // ========================================
+
+    // Create program & kernel.
+
+    // Create buffer
+
+    // Fence (out of order queue)
+
+    // Run kernel
+
+    // ========================================
+    window::main_loop(ocl_idle);
+    // ========================================
+
 
     clReleaseCommandQueue(queue);
     clReleaseContext(context);
     phree(platforms);
     phree(devices);
+
+    window::deinit();
 
     puts("Done.");
     return 0;
